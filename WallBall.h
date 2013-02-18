e75cdf16bbb6c932e4755da95c15985099298b66
Michael Madden <cs.utexas.edu>
2013-02-18 16:21:53 -0600

    first commit

---
+++ WallBall.h
@@ -0,0 +1,56 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    WallBall.h
+-----------------------------------------------------------------------------
+ 
+This source file is part of the
+   ___                 __    __ _ _    _ 
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/                              
+      Tutorial Framework
+      http://www.ogre3d.org/tikiwiki/
+-----------------------------------------------------------------------------
+*/
+#ifndef __WallBall_h_
+#define __WallBall_h_
+ 
+#include <OgreRoot.h>
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+#include <OgreWindowEventUtilities.h>
+ 
+class WallBall : public Ogre::WindowEventListener, public Ogre::FrameListener
+{
+public:
+    WallBall(void);
+    virtual ~WallBall(void);
+    bool go(void);
+ 
+protected:
+    // Ogre::WindowEventListener
+    virtual void windowResized(Ogre::RenderWindow* rw);
+    virtual void windowClosed(Ogre::RenderWindow* rw);
+ 
+    // Ogre::FrameListener
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+ 
+private:
+    Ogre::Root* mRoot;
+    Ogre::String mResourcesCfg;
+    Ogre::String mPluginsCfg;
+    Ogre::RenderWindow* mWindow;
+    Ogre::SceneManager* mSceneMgr;
+    Ogre::Camera* mCamera;
+ 
+    // OIS Input devices
+    OIS::InputManager* mInputManager;
+    OIS::Mouse*    mMouse;
+    OIS::Keyboard* mKeyboard;
+};
+ 
+#endif // #ifndef __WallBall_h_
