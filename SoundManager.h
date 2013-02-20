9ded2017419257dc4aa0c310a73703fe27859fe2
Michael Madden <cs.utexas.edu>
2013-02-20 17:43:45 -0600

    implementing SoundManager

--- SoundManager.h
+++ SoundManager.h
@@ -1,6 +1,24 @@
 //SoundManager.h
 
+#ifndef _SOUNDMANAGER_H_
+    #define _SOUNDMANAGER_H_
+
+#include <ResourceManager.h>
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <utilities.h>
+#include <string>
+#include <vector>
 
 class SoundManager{
-
+	private:
+		static SoundManager SoundControl;
+		vector<Mix_Chunk*> SoundList;
+	public:
+		int SoundManager::loadWAV(char nym[32]);
+		void SoundManager::playAudio();
+		void SoundManager::pauseAudio();
+		void SoundManager::playClip(int clipID, bool loop);
+		void SoundManager::mixAudio(void *unused, Uint8 *stream, Uint32 len);
+		void SoundManager::cleanup();
 }
