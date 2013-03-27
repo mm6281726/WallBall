93cc2c1d829b2808682cfc28b023aa161ff328fe
Christopher Black <cblack@cs.utexas.edu>
2013-03-26 20:42:42 -0500

    added network code

--- WallBall.cpp
+++ WallBall.cpp
@@ -787,10 +787,87 @@ void WallBall::windowClosed(Ogre::RenderWindow* rw)
 
 void NetworkHost(void)      //http://content.gpwiki.org/index.php/SDL:Tutorial:Using_SDL_net <- good code here, adapt messages accordingly
 {                           //http://jcatki.no-ip.org:8080/SDL_net/SDL_net.html <- good documentation here
-    TCPsocket sd, csd;      /* Socket descriptor, Client socket descriptor */
-    IPaddress ip, *remoteIP;
-    int quit, quit2;
-    char buffer[512];
+  TCPsocket sd, csd;      /* Socket descriptor, Client socket descriptor */
+  IPaddress ip, *remoteIP;
+  int quit, quit2;
+  char buffer[512];
+
+	if (SDLNet_Init() < 0)
+	{
+		fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError());
+		exit(EXIT_FAILURE);
+	}
+ 
+	/* Resolving the host using NULL make network interface to listen */
+	if (SDLNet_ResolveHost(&ip, NULL, 2000) < 0)
+	{
+		fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
+		exit(EXIT_FAILURE);
+	}
+ 
+	/* Open a connection with the IP provided (listen on the host's port) */
+	if (!(sd = SDLNet_TCP_Open(&ip)))
+	{
+		fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
+		exit(EXIT_FAILURE);
+	}
+ 
+	/* Wait for a connection, send data and term */
+	quit = 0;
+	while (!quit)
+	{
+		/* This check the sd if there is a pending connection.
+		* If there is one, accept that, and open a new socket for communicating */
+		if ((csd = SDLNet_TCP_Accept(sd)))
+		{
+			/* Now we can communicate with the client using csd socket
+			* sd will remain opened waiting other connections */
+ 
+			/* Get the remote address */
+			if ((remoteIP = SDLNet_TCP_GetPeerAddress(csd)))
+				/* Print the address, converting in the host format */
+				printf("Host connected: %x %d\n", SDLNet_Read32(&remoteIP->host), SDLNet_Read16(&remoteIP->port));
+			else
+				fprintf(stderr, "SDLNet_TCP_GetPeerAddress: %s\n", SDLNet_GetError());
+ 
+			quit2 = 0;
+			while (!quit2)
+			{
+				if (SDLNet_TCP_Recv(csd, buffer, 512) > 0)
+				{
+					printf("Client say: %s\n", buffer);
+ 
+					if(strcmp(buffer, "exit") == 0)	/* Terminate this connection */
+					{
+						quit2 = 1;
+						printf("Terminate connection\n");
+					}
+					if(strcmp(buffer, "quit") == 0)	/* Quit the program */
+					{
+						quit2 = 1;
+						quit = 1;
+						printf("Quit program\n");
+					}
+				}
+				//printf("Write something:\n>");
+				//scanf("%s", buffer);
+		 						//put message in buffer
+				len = strlen(buffer) + 1;
+				if (SDLNet_TCP_Send(csd, (void *)buffer, len) < len)
+				{
+					fprintf(stderr, "SDLNet_TCP_Send: %s\n", SDLNet_GetError());
+					exit(EXIT_FAILURE);
+				}
+
+			}
+ 
+			/* Close the client socket */
+			SDLNet_TCP_Close(csd);
+		}
+	}
+ 
+	SDLNet_TCP_Close(sd);
+	SDLNet_Quit();
 }
 
 void NetworkClient(void)    //http://content.gpwiki.org/index.php/SDL:Tutorial:Using_SDL_net <- good code here, adapt messages accordingly
